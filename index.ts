f348e51df85130ea2037493be5a1127ddfcd1111
Jordan Grieve <quintilex@gmail.com>
2020-01-24 10:08:06 -0500

    remove index

--- index.ts
+++
@@ -1,3 +0,0 @@
-export {EmojiService} from './src/emoji.service';
-export {EmojiInputComponent} from './src/emoji-input/emoji-input.component';
-export {EmojiPickerModule} from './emoji-picker.module';
